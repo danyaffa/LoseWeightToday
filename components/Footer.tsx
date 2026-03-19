6479eeaa5f91293261b72181edea68c73f13fd71
Dan Leffler <leffleryd@gmail.com>
2026-03-20 09:05:27 +1100

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>23 Salisbury St, Caulfield North, Melbourne 3161</p>
+      <p>ABN: 90124089345</p>
+      <p>Contact Jaffa: 0478 965 828</p>
+      <p>© Leffler International Investments Pty Ltd</p>
+    </footer>
+  );
+}

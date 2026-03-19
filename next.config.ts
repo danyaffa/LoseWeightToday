9c303f64803054ca5abbd81dce641b8780abc6a9
Dan Leffler <leffleryd@gmail.com>
2026-03-20 09:02:05 +1100

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ["images.squarespace-cdn.com", "weight-loss-shakes.goherbalife.com"],
+  },
+};
+
+export default nextConfig;
